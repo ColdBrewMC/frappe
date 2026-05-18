c1f25aa0f29e651b44b380c54e2db4cb747e3337
sylv256 <contact@sylv.gay>
2026-05-18 03:08:41 -0400

    docs: `SupportTier`

--- frappe-base/src/client/java/gay/sylv/frappe/api/base/extension/SupportTier.java
+++ frappe-base/src/client/java/gay/sylv/frappe/api/base/extension/SupportTier.java
@@ -10,9 +10,36 @@
 package gay.sylv.frappe.api.base.extension;
 
 /// The extent of support for a particular [RendererExtensionType] and its implementations.
+///
+/// The terms MUST, MUST NOT, SHOULD, SHOULD NOT, and MAY are used as defined in [RFC 2119](https://www.rfc-editor.org/rfc/rfc2119.html).
 public enum SupportTier {
+	/// An extension so essential that its implementation is required to be considered Frappé compliant.
+	/// Core extensions MUST NOT be added or removed outside minor Minecraft versions.
+	///
+	/// If a core extension is unimplemented in production, Frappé will crash.
+	///
+	/// All relevant conditions of [#STANDARD] apply unless otherwise specified.
 	CORE,
+	/// An extension based on a first party or de facto standard with widespread adoption.
+	/// Standard extensions MUST have a default implementation in Mocha.
+	///
+	/// All relevant conditions of [#NON_STANDARD] apply unless otherwise specified.
 	STANDARD,
+	/// A third party or otherwise non-standard extension with a default Sodium/Indigo implementation in
+	/// the mod it's defined in. An extension of this tier MAY have limited adoption, especially when
+	/// domain-specific.
+	///
+	/// In addition to SemVer restrictions, Standard extensions MUST NOT break API or ABI outside
+	/// minor Minecraft versions.
+	///
+	/// All relevant conditions of [#EXPERIMENTAL] apply unless otherwise specified.
 	NON_STANDARD,
+	/// A third party or otherwise experimental extension that may be unstable in nature or
+	/// have a rapidly evolving API. An extension of this tier MAY make breaking changes
+	/// as seen fit. However, all extensions SHOULD reduce breaking changes.
+	///
+	/// An experimental extension MUST NOT have a default implementation in Mocha.
+	/// Thus, default implementations of experimental extensions in Mocha are to
+	/// be disabled by default.
 	EXPERIMENTAL
 }
